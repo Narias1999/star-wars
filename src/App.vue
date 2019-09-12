5342da79da24159cb818efbc51126ea7543d55ed
Nicolás Arias <nicolasarias870@gmail.com>
2019-09-11 20:59:47 -0500

    results container

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="section">
+      <nav class="navbar is-spaced has-shadow">
+        <div class="container">
+          <input 
+            type="text"
+            @keypress.enter="search"
+            v-model="searchQuery"
+            class="input is-large"
+            placeholder="What are you looking for?"
+          >
+          <a class="button is-info is-large" @click="search">Search</a>
+          <a class="button is-danger is-large">&times;</a>
+        </div>
+      </nav>
+
+      <div class="container results">
+        <div class="columns">
+          <div class="column" v-for="character in characters" :key="character.name">
+            {{character.name}}
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const characters = [
+  { name: 'Luke', world: 'skywoker' }
+]
 export default {
   name: 'app',
+
+  data() {
+    return {
+      searchQuery: '',
+      characters: []
+    }
+  },
+
+  methods: {
+    search() {
+       console.log(this.searchQuery)
+      this.characters = characters.filter(character => character.name.search(this.searchQuery) > -1)
+    }
+  },
+
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
   @import './scss/main.scss';
+  .results {
+    margin-top: 30px;
+  }
 </style>
